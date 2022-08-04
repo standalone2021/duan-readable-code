fcb3b5798843cb4aad71dc0ec72eb2dd514cd639
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-08-04 16:05:55 +0900

    spec5: 辞書の単語にIDを追加しました

--- task1.py
+++ task1.py
@@ -1,7 +1,9 @@
 #spec1
 #print('上手')
 
-#spec2
+#spec5
 with open("/Users/名前/Downloads/dictionary-data.txt") as dictionary_file:
-    word = dictionary_file.read()
-    print(word)
+    dictionary = dictionary_file.read()
+    word_list = dictionary.split("\n")
+    for id in range(len(word_list)):
+        print("{}: {}".format(id, word_list[id]))
